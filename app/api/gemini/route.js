c378e228cb133c03b33d447aa39b4e5610074c66
Nick-ui911 <nbaghel392@gmail.com>
2026-01-01 14:42:52 +0530

    dynamic added in gemini api

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { cookies } from "next/headers";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { prisma } from "@/lib/prisma";
