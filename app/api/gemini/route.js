667dbe2154faf2f51b964b3bf4ec123d3b03ea19
Nick-ui911 <nbaghel392@gmail.com>
2026-01-01 15:01:03 +0530

    ..

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { prisma } from "@/lib/prisma";
 import { userAuth } from "../../middleware/userAuth";
